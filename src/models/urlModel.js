ac422c8cbfdd2a2dc463d081b1d5693dc10bed1a
PritalRupnur <pritalrupnur29012000@gmail.com>
2022-09-29 11:50:42 +0530

    route and models

---
+++ src/models/urlModel.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+
+const urlSchema = new mongoose.Schema({
+
+    urlCode: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true
+    },
+    longUrl: {
+        type: String,
+        required: true
+    },//valid url
+    shortUrl: {
+        type: String,
+        required: true,
+        unique: true
+    }
+
+
+}, { timestamps: true })
+
+module.exports=mongoose.model("url", urlSchema)
+
+
+//{ urlCode: { mandatory, unique, lowercase, trim }, longUrl: {mandatory, valid url}, shortUrl: {mandatory, unique} }
---
+++ src/route/route.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const router = express.Router()
+const urlController = require('../controllers/urlController')
+
+
+router.post("/test-me")
+
+module.exports = router
