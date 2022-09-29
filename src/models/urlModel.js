9c3e7ce1d31df8d1cba5efe2c74c8de7db61cab7
shreya <10shreyad@gmail.com>
2022-09-29 11:57:06 +0530

    index created

--- src/models/urlModel.js
+++ src/models/urlModel.js
@@ -1,28 +1,27 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-const urlSchema = new mongoose.Schema({
-
+const urlSchema = new mongoose.Schema(
+  {
     urlCode: {
-        type: String,
-        required: true,
-        unique: true,
-        lowercase: true,
-        trim: true
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
     },
     longUrl: {
-        type: String,
-        required: true
-    },//valid url
+      type: String,
+      required: true,
+    }, //valid url
     shortUrl: {
-        type: String,
-        required: true,
-        unique: true
-    }
+      type: String,
+      required: true,
+      unique: true,
+    },
+  },
+  { timestamps: true }
+);
 
-
-}, { timestamps: true })
-
-module.exports=mongoose.model("url", urlSchema)
-
+module.exports = mongoose.model("url", urlSchema);
 
 //{ urlCode: { mandatory, unique, lowercase, trim }, longUrl: {mandatory, valid url}, shortUrl: {mandatory, unique} }
