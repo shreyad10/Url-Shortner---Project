ad64e59a5faf381f3289005ae5fa0348c557860d
Arindam <102957273+babudan@users.noreply.github.com>
2022-10-04 23:42:49 +0530

    final

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 const express = require("express")
-const bodyParser = require("body-parser")
 const route = require('./route/route.js');
 const { default: mongoose } = require('mongoose');
 const app = express();
